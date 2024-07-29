5a2ce60f2274d0348ba69ac10af7bbd3dbbf4c16
maedahnazar <maedahnazar@gmail.com>
2024-07-29 10:27:01 +0500

    generic spiders

--- pakwheels_project/pakwheels_project/spiders/pakwheels_spider.py
+++ pakwheels_project/pakwheels_project/spiders/pakwheels_spider.py
@@ -1,50 +1,32 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from .extractors import PakWheelsExtractor
 
 
-class PakWheelsSpider(scrapy.Spider):
+class PakWheelsSpider(CrawlSpider):
     name = 'pakwheels'
     allowed_domains = ['pakwheels.com']
     start_urls = ['https://www.pakwheels.com/used-cars/search/-/']
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(
-                url=url,
-                callback=self.parse_root_url,
-                cookies={"countrycurrency": "PKR"}
-            )
-
-    def parse_root_url(self, response):
-        ad_links = response.css('div.col-md-9.search-listing.pull-right a.car-name.ad-detail-path::attr(href)').getall()
-        
-        for link in ad_links:
-            yield response.follow(link, self.parse_ad_details)
-
-        next_page = response.css('ul.pagination li a[rel="next"]::attr(href)').get()
-        if next_page:
-            yield response.follow(next_page, self.parse_root_url)
+    rules = (
+        Rule(
+            LinkExtractor(restrict_css='div.col-md-9.search-listing.pull-right a.car-name.ad-detail-path'), 
+            callback='parse_ad_details'
+        ),
+        Rule(LinkExtractor(restrict_css='ul.pagination li a[rel="next"]'), follow=True),
+    )
 
     def parse_ad_details(self, response):
-        extractor = PakWheelsExtractor()
-        title = extractor.extract_title(response)
-        images = extractor.extract_images_links(response)
-        location = extractor.extract_location(response)
-        price = extractor.extract_price(response)
-        car_details = extractor.extract_car_details(response)
-        car_features = extractor.extract_car_features(response)
-        seller_comments = extractor.extract_seller_comments(response)
-        inspection_report = extractor.extract_inspection_report(response)
-        
         yield {
-            'title': title,
-            'location': location,
-            'price': price,
-            'images': images,
-            'car_details': car_details,
-            'car_features': car_features,
-            'seller_comments': seller_comments,
-            'inspection_report': inspection_report,
+            'title': PakWheelsExtractor.extract_title(response),
+            'location': PakWheelsExtractor.extract_location(response),
+            'price': PakWheelsExtractor.extract_car_price(response),
+            'images': PakWheelsExtractor.extract_car_images_links(response),
+            'car_details': PakWheelsExtractor.extract_car_details(response),
+            'car_features': PakWheelsExtractor.extract_car_features(response),
+            'seller_comments': PakWheelsExtractor.extract_car_seller_comments(response),
+            'inspection_report': PakWheelsExtractor.extract_inspection_report(response),
             'link': response.url,
         }
