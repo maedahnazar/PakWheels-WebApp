41dd268e78495ecb01d7bcfe80cef587da02f1a7
maedahnazar <maedahnazar@gmail.com>
2024-07-30 11:37:43 +0500

    Pakwheels project startup

---
+++ pakwheels_webapp/user_accounts/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user_accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to a success page.
+    else:
+        form = AuthenticationForm()
+    return render(request, 'user_accounts/login.html', {'form': form})
