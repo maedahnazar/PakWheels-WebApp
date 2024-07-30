d0bbeada2966151176ff74dabe032452f06ea59f
maedahnazar <maedahnazar@gmail.com>
2024-07-30 15:41:55 +0500

    User login/ signup

---
+++ pakwheels_webapp/accounts/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
