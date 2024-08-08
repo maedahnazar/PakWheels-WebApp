9ca2d7e5ba4ee9aa70bb71a55d67c2e9ee04c621
maedahnazar <maedahnazar@gmail.com>
2024-08-08 12:00:24 +0500

    Views updated

--- pakwheels_app_project/ads/views.py
+++ pakwheels_app_project/ads/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods
 
 from ads.models import Ad
 
 
+@require_http_methods(["GET"])
 def home(request):
     return render(request, 'ads/home.html', {'ads': Ad.objects.all()})
 
+@require_http_methods(["GET"])
 def ad_detail(request, ad_id):
     return render(request, 'ads/ad_detail.html', {'ad': get_object_or_404(Ad, id=ad_id)})
